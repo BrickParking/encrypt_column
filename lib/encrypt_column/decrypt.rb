ff361151a51df6dd5ebfe1772b6f00f6075b2a70
Botond Erdos <erdosboti@gmail.com>
2021-03-24 14:09:42 +0100

    handle empty and nil password fix

--- lib/encrypt_column/decrypt.rb
+++ lib/encrypt_column/decrypt.rb
@@ -10,7 +10,7 @@ class Decrypt
 
   def self.ciphertext(ciphertext, key = ENV['ENCRYPT_KEY'])
     raise 'Encryption Key Config Missing' unless key.present?
-    return 'ERROR: Missing encryption ciphertext' if ciphertext.nil? || ciphertext.blank?
+    return if ciphertext.blank?
     enciphered, iv = ciphertext.split('--', 2).map { |part| part.unpack('m')[0] }
     decipher = OpenSSL::Cipher::AES256.new(:CBC)
 
